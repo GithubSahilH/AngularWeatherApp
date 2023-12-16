63a52a75b96538f7783a8d2b548fca3e6dd8e9d1
Ghostsahil666 <mohammadsahilbhat01@gmail.com>
2023-12-16 15:08:54 +0530

    hi

---
+++ WeatherAppAngular/src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'b65197c2a9mshd869c62f65e62d4p1aa576jsn17071239f739',
+
+};
